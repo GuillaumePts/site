01a6bea91e7dbc682d3491ecae062e36f835e0c7
Guillaume Pitois <guillaume.pitois1@gmail.com>
2024-08-11 23:42:44 +0200

    modif profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,12 +7,14 @@
 
 <div id="moncontainer" >
 
-
-    <div id="card">
+    <div id="me">
         <a class="contact" href="#footer">Contactez moi</a>
-        <figure>
-            <img src="../assets/img/moichameau.jpg" alt="">
-        </figure>
+        <div id="containercard">
+            <figure>
+                <img src="../assets/img/1000003979.jpg" alt="photo du propriétaire du site">
+            </figure>
+            <div id="card">
+        
         <div>
             
                 <p><span class="material-symbols-outlined">person</span> Pitois Guillaume, 22 ans</p>
@@ -30,13 +32,17 @@
             
         </div>
 
-            <a id="cv" class="contact" href="cv" download="CV_Pitois_Guillaume.pdf">
+            <a id="cv" class="contact" href="cv" download="cv2024.pdf">
                 Télécharger mon CV <span class="material-symbols-outlined">download</span>
             </a>
         
 
 
     </div>
+        </div>
+        
+    </div>
+
 </div>
 
 
@@ -55,21 +61,40 @@
     flex-direction: column;
     overflow: hidden;
     background-color: #e8e8e8;
+    
 
 }
 
+#me{
+    width: 320px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+}
 
+#me figure{
+    width: 100%;
+}
+
+#me figure img{
+    width: 100%;
+    box-shadow: 15px 15px 30px #bebebe,-15px -15px 30px #ffffff;
+    border-radius: 20px;
+}
 
 #card{
     box-shadow: 15px 15px 30px #bebebe,-15px -15px 30px #ffffff;
     transition: 0.2s ease-in-out;
-    border-radius: 20px;
+    /* border-radius: 20px; */
     width: 90%;
     max-width: 350px;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
     position: relative;
+
 }
 
 #card #cv{
@@ -99,23 +124,7 @@
     box-shadow: 0px 10px 20px rgba(0,0,0,0.1);
 }
 
-#card figure{
-    width: 100%;
-    height: 250px;
-    position: relative;
-    border-top-right-radius: 20px;
-    border-top-left-radius: 20px;
-    overflow: hidden;
-}
 
-#card figure img{
-    width: 100%;
-    border-top-left-radius: 20px;
-    position: absolute;
-    top: -30px;
-    
-    
-}
 
 #card div{
     margin: 20px 10px;
@@ -130,45 +139,48 @@
 
 @media (min-width: 913px){
     #card{
-        margin-top: 60px;
-        max-width: 500px;
-        height: 470px;
+        
+        max-width: 370px;
+        height: 420px;
+        margin: 0;
     }
 
-    #card figure{
-        width: 100%;
-        height: 250px;
-        
-        position: relative;
-        border-top-right-radius: 20px;
-        border-top-left-radius: 20px;
+    #moncontainer{
+        gap: 30px;
+        border: 1px solid blue;
     }
 
-    #card figure img{
-        width: 100%;
-        /* filter: grayscale(50%) */
-        border-top-left-radius: 20px;
-        position: absolute;
-        top: -120px;
-        
-    }   
+    #me{
+        width: 320px;
+    }
 
-    #card div{
-    margin: 10px 5px;
-}
+    #me figure{
+        width: 100%;
+    }
+
+    #me figure img{
+        width: 100%;
+        box-shadow: 15px 15px 30px #bebebe,-15px -15px 30px #ffffff;
+    }
+
+    #me figure:hover{
+    cursor: pointer;
+    box-shadow: 0px 10px 20px rgba(0,0,0,0.1);
+    }
+
+    #containercard{
+        display: flex;
+        width: 600px;
+        border: 1px solid red;
+    }
+
+
+
 }
 
 
 @media (min-width: 1113px){
-    #card{
-        max-width: 550px;
-        height: 600px;
-    }
-
-
-    #card div{
-    margin: 20px 10px;
-    }
+   
 }
 
 
